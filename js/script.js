a93437ff295c10faa11451947b1387eaba8f85d3
DRONARTHUR <113294846+DRONARTHUR@users.noreply.github.com>
2022-10-12 00:15:43 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function startGame(){
     document.querySelector(".menu-game").style.display = "none";
 }
+function deliveryMenu(){
+    if(document.querySelector(".delivery").style.display != "block"){
+        document.querySelector(".delivery").style.display = "block";
+    }
+}
+function closeWindow(whoClose){
+    if(whoClose == 'deliveryWindow'){
+        document.querySelector(".delivery").style.display = "none";
+    }
+}
