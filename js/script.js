958bc8ccd1b603b8a5145bc8957199e5802b3525
DRONARTHUR <113294846+DRONARTHUR@users.noreply.github.com>
2022-10-18 18:29:24 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
+var money;
+localStorage.setItem('moneylocal', 10000);
+money = parseInt(localStorage.getItem('moneylocal'));
+localStorage.setItem('moneylocal', money);
+document.querySelector('.moneyNumber').innerHTML = money + ' ';
+
+function mouseSound(){
+    var audio = new Audio('sound/mouseClick.mp3');
+    audio.play();
+}
 function startGame(){
     document.querySelector(".menu-game").style.display = "none";
 }
 function deliveryMenu(){
     if(document.querySelector(".delivery").style.display != "block"){
+        document.querySelector(".warehouse-info").style.display = "none";
         document.querySelector(".delivery").style.display = "block";
     }
 }
+function warehouseMenu(){
+    if(document.querySelector(".warehouse-info").style.display != "block"){
+        document.querySelector(".delivery").style.display = "none";
+        document.querySelector(".warehouse-info").style.display = "block";
+    }
+}
 function closeWindow(whoClose){
-    if(whoClose == 'deliveryWindow'){
+    if(whoClose == 'infoWindow'){
+        document.querySelector(".warehouse-info").style.display = "none";
         document.querySelector(".delivery").style.display = "none";
     }
 }
